3f83a82712aea2bbecbfe4adb5aecd7a6f4ac446
aheshiyu <division.a614@gmail.com>
2022-11-05 00:03:25 +0900

    add night rain condition

---
+++ src/components/Kokoro.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="(monster, key) in monster_list" :key="key" class="pa-0" cols=auto>
+        <v-img
+          v-if="monster"
+          max-height="35"
+          max-width="35"
+          :src="monster.image_path"
+          :class="{ 'condition_night': monster.is_night, 'condition_rain': monster.is_rain }"
+        ></v-img>
+        <!-- <div>{{monster.is_rain}}</div> -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  name: 'Kokoro',
+
+  props: [
+    'monster_list'
+  ],
+
+  data: () => ({
+
+  }),
+}
+</script>
+
+<style>
+  .condition_night {
+    background-color: darkblue;
+  }
+  .condition_rain {
+    background-color: aqua;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>  
-  <v-col class="pa-0; height:100px">
+  <v-col>
     <v-data-table
       :headers="headers"
       :items="datas"
@@ -18,128 +18,31 @@
       </template>
 
       <template v-slot:item.rare1="props">
-        <td class="pl-1 pr-1">
-          <v-container>
-            <v-row>
-              <v-col v-for="(monster, key) in props.item.rare1" :key="key" class="pa-0" cols=auto>
-                <v-img
-                  v-if="monster"
-                  max-height="35"
-                  max-width="35"
-                  :src="monster.image_path"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </td>
+        <kokoro :monster_list="props.item.rare1" class="pa-0"></kokoro>
       </template>
 
       <template v-slot:item.rare2="props">
-        <td class="pl-1 pr-1">
-          <v-container>
-            <v-row>
-              <v-col v-for="(monster, key) in props.item.rare2" :key="key" class="pa-0" cols=auto>
-                <v-img
-                  v-if="monster"
-                  max-height="35"
-                  max-width="35"
-                  :src="monster.image_path"
-                  class="mr-0"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </td>
+        <kokoro :monster_list="props.item.rare2" class="pa-0"></kokoro>
       </template>
 
       <template v-slot:item.rare3="props">
-        <td class="pl-1 pr-1">
-          <v-container>
-            <v-row>
-              <v-col v-for="(monster, key) in props.item.rare3" :key="key" class="pa-0" cols=auto>
-                <v-img
-                  v-if="monster"
-                  max-height="35"
-                  max-width="35"
-                  :src="monster.image_path"
-                  class="mr-0"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </td>
+        <kokoro :monster_list="props.item.rare3" class="pa-0"></kokoro>
       </template>
 
       <template v-slot:item.rare4="props">
-        <td class="pl-1 pr-1">
-          <v-container>
-            <v-row>
-              <v-col v-for="(monster, key) in props.item.rare4" :key="key" class="pa-0" cols=auto>
-                <v-img
-                  v-if="monster"
-                  max-height="35"
-                  max-width="35"
-                  :src="monster.image_path"
-                  class="mr-0"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </td>
+        <kokoro :monster_list="props.item.rare4" class="pa-0"></kokoro>
       </template>
 
       <template v-slot:item.rare5="props">
-        <td class="pl-1 pr-1">
-          <v-container>
-            <v-row>
-              <v-col v-for="(monster, key) in props.item.rare5" :key="key" class="pa-0" cols=auto>
-                <v-img
-                  v-if="monster"
-                  max-height="35"
-                  max-width="35"
-                  :src="monster.image_path"
-                  class="mr-0"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </td>
+        <kokoro :monster_list="props.item.rare5" class="pa-0"></kokoro>
       </template>
 
       <template v-slot:item.rare6="props">
-        <td class="pl-1 pr-1">
-          <v-container>
-            <v-row>
-              <v-col v-for="(monster, key) in props.item.rare6" :key="key" class="pa-0" cols=auto>
-                <v-img
-                  v-if="monster"
-                  max-height="35"
-                  max-width="35"
-                  :src="monster.image_path"
-                  class="mr-0"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </td>
+        <kokoro :monster_list="props.item.rare6" class="pa-0"></kokoro>
       </template>
 
       <template v-slot:item.region="props">
-        <td class="pl-1 pr-1">
-          <v-container>
-            <v-row>
-              <v-col v-for="(monster, key) in props.item.region" :key="key" class="pa-0" cols=auto>
-                <v-img
-                  v-if="monster"
-                  max-height="35"
-                  max-width="35"
-                  :src="monster.image_path"
-                  class="mr-0"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </td>
+        <kokoro :monster_list="props.item.region" class="pa-0"></kokoro>
       </template>
 
     </v-data-table>
@@ -147,13 +50,13 @@
 </template>
 
 <script>
-// import HelloWorld from "../components/HelloWorld"
+import Kokoro from "../components/Kokoro.vue"
 
 export default {
   name: 'Home',
 
   components: {
-    // HelloWorld
+    Kokoro
   },
 
   data() {
@@ -163,10 +66,10 @@ export default {
         { text: "めったに", value: "rare5", sortable: false, width: 60, class: 'pa-1' },
         { text: "あまり", value: "rare4", sortable: false, width: 80, class: 'pl-1' },
         { text: "メタル", value: "rare6", sortable: false, width: 80, class: 'pl-1' },
-        { text: "ときどき", value: "rare3", sortable: false, width: 115, class: 'pl-1' },
-        { text: "ご当地", value: "region", sortable: false, width: 60, class: 'pa-1' },
+        { text: "ときどき", value: "rare3", sortable: false, width: 120, class: 'pl-1' },
+        { text: "ご当地", value: "region", sortable: false, width: 50, class: 'pa-1' },
         { text: "よく", value: "rare2", sortable: false, width: 150, class: 'pl-1' },
-        { text: "とても", value: "rare1", sortable: false, width: 330, class: 'pl-1' },
+        { text: "とてもよく", value: "rare1", sortable: false, width: 330, class: 'pl-1' },
       ],
       datas: [],
     }
@@ -180,7 +83,7 @@ export default {
     const res = await this.$axios.fetch()
     const raw_story = res.data.story
     const raw_monster = res.data.monster
-    
+
     const num2monster = (n) => {
         if (n == '') return
         let monster = raw_monster.find(m => n == m.monster_id)
@@ -225,10 +128,4 @@ export default {
   max-width: 200px
   overflow: hidden
   text-overflow: ellipsis
-</style>
-
-<style>
-  html {
-    height: 100%
-  }
 </style>
